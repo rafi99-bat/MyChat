e945a7c8423b000133350cea2b0a6ac3f1026ccb
Rafeed Mahbub <rafeedmahbubrafi007@gmail.com>
2021-06-21 08:52:41 +0600

    Day 8

--- src/com/project/mychat/Client.java
+++ src/com/project/mychat/Client.java
@@ -20,6 +20,12 @@ import java.awt.event.ActionEvent;
 import javax.swing.JTextField;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
 
 public class Client extends JFrame {
 	private static final long serialVersionUID = 1L;
@@ -30,14 +36,64 @@ public class Client extends JFrame {
 	private JTextField txtMessage;
 	private JTextArea history;
 	private DefaultCaret caret;
+	
+	private DatagramSocket socket;
+	private InetAddress ip;
+	
+	private Thread send;
 
 	public Client(String name, String address, int port) {
 		setTitle("MyChat Client");
 		this.name = name;
 		this.address = address;
 		this.port = port;
+		boolean connect = openConnection(address, port);
+		if(!connect) {
+			System.err.println("Connection Failed!");
+			console("Connection Failed!");
+		}
 		createWindow();
 		console("Attempting a connection to " + address + ": " + port + ", user: " + name);
+	}
+	
+	private boolean openConnection(String address, int port) {
+		try {
+			socket = new DatagramSocket();
+			ip = InetAddress.getByName(address);
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+			return false;
+		} catch (SocketException e) {
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
+	
+	private String receive() {
+		byte[] data = new byte[1024];
+		DatagramPacket packet = new DatagramPacket(data, data.length);
+		try {
+			socket.receive(packet);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		String message = new String(packet.getData());
+		return message;
+	}
+	
+	private void send(final byte[] data) {
+		send = new Thread("Send") {
+			public void run() {
+				DatagramPacket packet = new DatagramPacket(data, data.length, ip, port);
+				try {
+					socket.send(packet);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		};
+		send.start();
 	}
 	
 	private void createWindow() {
@@ -68,9 +124,10 @@ public class Client extends JFrame {
 		GridBagConstraints scrollConstraints = new GridBagConstraints();
 		scrollConstraints.insets = new Insets(0, 0, 5, 5);
 		scrollConstraints.fill = GridBagConstraints.BOTH;
-		scrollConstraints.gridx = 1;
-		scrollConstraints.gridy = 1;
-		scrollConstraints.gridwidth = 2;
+		scrollConstraints.gridx = 0;
+		scrollConstraints.gridy = 0;
+		scrollConstraints.gridwidth = 3;
+		scrollConstraints.gridheight = 2;
 		scrollConstraints.insets = new Insets(0, 5, 0, 0); 
 		contentPane.add(scroll, scrollConstraints);
 		
@@ -85,8 +142,9 @@ public class Client extends JFrame {
 		GridBagConstraints gbc_txtMessage = new GridBagConstraints();
 		gbc_txtMessage.insets = new Insets(0, 0, 0, 5);
 		gbc_txtMessage.fill = GridBagConstraints.HORIZONTAL;
-		gbc_txtMessage.gridx = 1;
+		gbc_txtMessage.gridx = 0;
 		gbc_txtMessage.gridy = 2;
+		gbc_txtMessage.gridwidth = 2;
 		contentPane.add(txtMessage, gbc_txtMessage);
 		txtMessage.setColumns(10);
 		
